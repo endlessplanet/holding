94f4e65e426175d541a4a5fa48f7843928d32628
Emory Merryman <emory.merryman@gmail.com>
2017-09-12 23:59:16 +0000

    not sure

--- bin/exec.sh
+++ bin/exec.sh
@@ -6,6 +6,7 @@ CID=$(mktemp) &&
             docker rm --force --volume $(cat ${CID}) &&
             rm -f ${CID}
     } &&
+    (nohup ssh -i bin/blacklogbook.pem -fN -L 0.0.0.0:25139:0.0.0.0:8181 sshd </dev/null >/tmp/sshd1.log 2>&1 &) &&
     docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD} &&
     docker image pull endlessplanet/holding:$(git rev-parse --verify HEAD) &&
     rm -f ${CID} &&
