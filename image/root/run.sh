a4d27337228f6469349ebe1eb9c315922905ec67
Emory Merryman <emory.merryman@gmail.com>
2017-09-13 00:00:23 +0000

    not sure

--- image/root/run.sh
+++ image/root/run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-apk update &&
-    apk upgrade &&
-    adduser -D user &&
-    mkdir /home/user/volumes &&
-    mkdir /home/user/volumes/gitlab &&
-    mkdir /home/user/volumes/gitlab/config &&
-    mkdir /home/user/volumes/gitlab/logs &&
-    mkdir /home/user/volumes/gitlab/data &&
-    chown -R user:user /home/user/volumes &&
-    rm -rf /var/cache/apk/*
+dnf update --assumeyes &&
+    dnf install --assumeyes git make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ nodejs &&
+    mkdir /opt/docker/c9sdk &&
+    git -C /opt/docker/c9sdk init &&
+    git -C /opt/docker/c9sdk remote add origin git://github.com/c9/core.git &&
+    git -C /opt/docker/c9sdk pull origin master &&
+    /opt/docker/c9sdk/scripts/install-sdk.sh &&
+    cp /opt/docker/docker.repo /etc/yum.repos.d/ &&
+    dnf update --assumeyes &&
+    dnf install --assumeyes docker-engine &&
+    dnf install --assumeyes util-linux-user &&
+    dnf update --assumeyes &&
+    dnf clean all &&
+    true
